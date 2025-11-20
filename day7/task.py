0cfc083344dab1a97b33544480e1f54c19cebbd1
wizybasil <wizybasil@gmail.com>
2025-11-20 18:36:17 +0100

    Update task.py
    Assignment Done BY WISDOM

--- day7/task.py
+++ day7/task.py
@@ -5,3 +5,45 @@ multiply(a, b): returns the product of a and b
 power(a, b): returns a raised to the power of b
 subtract(a, b): returns the difference of a and b
 """
+# 1.
+
+
+def sum(a, b):
+    sum = a + b
+    return sum
+
+
+x = sum(4, 2)
+print(x)
+
+# # 2.
+
+
+def product_of_two_num(a, b):
+    product_of_two_num = a * b
+    return product_of_two_num
+
+
+x = product_of_two_num(2, 2)
+print(x)
+
+# 3.
+
+
+def power(a, b):
+    power = (a ** b)
+    return power
+
+
+x = power(4, 2)
+print(x)
+
+
+# 4.
+def subtract(a, b):
+    subtract = a - b
+    return a - b
+
+
+x = subtract(4, 2)
+print(x)
